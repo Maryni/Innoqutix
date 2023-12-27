c102a8530fdf2f4e7b4e236bc327aa24cf5c679a
Andrew <prikolchik574@gmail.com>
2023-12-27 17:46:21 +0200

    add jumpController, some movement add right/left checker need to config JumpMod value

---
+++ Assets/Scripts/Controllers/MoveController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveController : MonoBehaviour
+{
+    [SerializeField] private Rigidbody rigidbody;
+    [SerializeField] private float jumpMod;
+    [SerializeField] private float rightMoveMod;
+    [SerializeField] private float leftMoveMod;
+
+    private Coroutine jumpCoroutine;
+
+    public void DoJump()
+    {
+        rigidbody.AddForce(Vector2.up * jumpMod);
+    }
+
+    public void DoRightMove()
+    {
+        rigidbody.AddForce(Vector2.right * rightMoveMod * Time.deltaTime);
+    }
+
+    public void DoLeftMove()
+    {
+        rigidbody.AddForce(Vector2.left * leftMoveMod * Time.deltaTime);
+    }
+
+    private void Jump()
+    {
+
+        //StopCoroutine(jumpCoroutine);
+        //jumpCoroutine = null;
+        //yield break;
+    }
+}
--- Assets/Scripts/Game/Player.cs
+++ Assets/Scripts/Game/Player.cs
@@ -4,7 +4,43 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] private JumpController jumpController;
+    [SerializeField] private MoveController moveController;
+    private Vector2 touchPos;
 
+    private void Start()
+    {
+        moveController.DoJump();
+    }
 
+    private void FixedUpdate()
+    {
+        if(Input.touchCount >0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            Touch touch = Input.GetTouch(0);
+            touchPos = touch.position;
+        }
+
+        if (IsRightPartOfScreen())
+        {
+            moveController.DoRightMove();
+        }
+        else
+        {
+            moveController.DoLeftMove();
+        }
+    }
+
+    private void GetTouchPosition()
+    {
+        Touch touch = Input.GetTouch(0);
+    }
+
+    private bool IsRightPartOfScreen()
+    {
+        if(touchPos.x > (Screen.width /2))
+        {
+            return true;
+        }
+        return false;
+    }
 }
